026bae1a2cee9f17f9936f6808db1b1d57a710e7
EmmaRosby <U2457534@rts-365.dk>
2025-05-19 08:53:21 +0200

    fix: Update allowed origin in CORS configuration and correct import case for App component

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   const allowedOrigins = [
-    "liveside-link",
+    "https://bornelejren-pa-langeland-emmarosby.onrender.com",
     "http://localhost:5173"
   ];
   const origin = req.headers.origin;
